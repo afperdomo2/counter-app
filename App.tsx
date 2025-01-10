9ff3e5d763aabf64ed0cec389c949b49d7a3f368
alvarado0925 <alvarado0925@gmail.com>
2025-01-09 23:44:14 -0500

    Agregar configuración de Prettier y funcionalidad de contador en App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  Touchable,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(count + 10)}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +35,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    elevation: 3,
   },
 });
